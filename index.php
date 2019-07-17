72efbdac49d6d9fc31338fa1a428f7a980e4a572
8Kunoir8 <steruzzvalentine@gmail.com>
2019-07-17 14:43:39 +0700

    change connection

--- index.php
+++ index.php
@@ -26,32 +26,32 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    // $host = "dicodingserver.database.windows.net";
-    // $user = "dicodingadmin";
-    // $pass = "D1c0d1ng";
-    // $db = "dicodingsql";
+    $host = "dicodingserver.database.windows.net";
+    $user = "dicodingadmin";
+    $pass = "D1c0d1ng";
+    $db = "dicodingsql";
 
-    // try {
-    //     $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
-    //     $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-    // } catch(Exception $e) {
-    //     echo "Failed: " . $e;
-    // }
+    try {
+        $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
+        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    } catch(Exception $e) {
+        echo "Failed: " . $e;
+    }
 
     // PHP Data Objects(PDO) Sample Code:
-    try {
-        $conn = new PDO("sqlsrv:server = tcp:dicodingserver.database.windows.net,1433; Database = dicodingsql", "dicodingadmin", "D1c0d1ng");
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    }
-    catch (PDOException $e) {
-        print("Error connecting to SQL Server.");
-        die(print_r($e));
-    }
+    // try {
+    //     $conn = new PDO("sqlsrv:server = tcp:dicodingserver.database.windows.net,1433; Database = dicodingsql", "dicodingadmin", "D1c0d1ng");
+    //     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // }
+    // catch (PDOException $e) {
+    //     print("Error connecting to SQL Server.");
+    //     die(print_r($e));
+    // }
 
-    // SQL Server Extension Sample Code:
-    $connectionInfo = array("UID" => "dicodingadmin@dicodingserver", "pwd" => "D1c0d1ng", "Database" => "dicodingsql", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-    $serverName = "tcp:dicodingserver.database.windows.net,1433";
-    $conn = sqlsrv_connect($serverName, $connectionInfo);
+    // // SQL Server Extension Sample Code:
+    // $connectionInfo = array("UID" => "dicodingadmin@dicodingserver", "pwd" => "D1c0d1ng", "Database" => "dicodingsql", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+    // $serverName = "tcp:dicodingserver.database.windows.net,1433";
+    // $conn = sqlsrv_connect($serverName, $connectionInfo);
 
     if (isset($_POST['submit'])) {
         try {
